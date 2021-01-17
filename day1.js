02f6ec4dc5c85c981ff87b8d1125092d7933d8e9
Caitlin Wright <caitlinwright@Caitlins-MacBook-Pro.local>
2021-01-16 19:11:09 -0800

    Day 1 solution added.

---
+++ day1.js
@@ -0,0 +1,22 @@
+// Prompt:
+
+// n children have m pieces of candy. They want to eat as much candy as they can,
+// but each child must eat exactly the same number of candies. Individual pieces of
+// candy cannot be split.
+
+// Example: 
+
+// For n = 3 and m = 10, the output should be 9
+// Each child will each 3 pieces of candy.
+
+// Hints:
+
+// math.floor
+
+// Plan of Action: 
+// Take the total number of candy and divide by the number of children.
+// The integer result is the number of candidates each child will get.
+
+function candies(children, candy) {
+    return parseInt(candy/children)*children // parseInt converts the number to the lowest integer
+}
