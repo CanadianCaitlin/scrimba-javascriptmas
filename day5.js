6d621eda9afb2fa437d1e255f83c0d470c88864c
Caitlin Wright <caitlinwright@Caitlins-MacBook-Pro.local>
2021-01-17 00:50:50 -0800

    Day 5 solution added.

---
+++ day5.js
@@ -0,0 +1,24 @@
+// Prompt:
+
+// Reverse the provided string. You may need to convert the string into an array before you are reverse.
+// Your result must be a string.
+
+// Example: 
+// reverseAString('hello') returns 'olleh'
+// reverseAString('Howdy') returns 'ydwoH'
+
+// Hints:
+// join(): method creates and returns a new string by concatenating all of the elements in an array
+// split(): split a string into an array of substrings
+// reverse(): method reverses an array in place
+
+function reverseAString(str) {
+    wordArray = [] // create an empty array for each letter to be added to in order
+    for (let i = 0; i < str.length; i++) {
+        wordArray.push(str[i])
+    }
+    reversedArray = wordArray.reverse() // reverse the order of the letters in the list
+    word = reversedArray.join('') // join the reversed list of letters into one string
+    return word
+}
+
