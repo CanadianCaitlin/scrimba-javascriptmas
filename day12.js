3ca3e905d65d5594fcca6f07dc1cb3954f023e8e
Caitlin Wright <caitlinwright@Caitlins-MacBook-Pro.local>
2021-01-18 21:29:59 -0800

    Day 12 solution added.

---
+++ day12.js
@@ -0,0 +1,18 @@
+// Prompt:
+
+// Check if the given string is a correct time representation of the 24-hour clock.
+
+// Example:
+
+// For time = "13.58", the output should be validTime(time) = true
+// For time = "25:51", the output should be validTime(time) = false
+// For time = "02.76", the output should be validTime(time) = false
+
+function validTime(str) {
+    let timeSplit = str.split(":") // splits string by :
+    if (timeSplit[0] > 24 || timeSplit[1] > 59) { // if hour is greater than 24 or minutes is greater than 59, return false
+        return false
+    } else {
+        return true
+    }
+}
