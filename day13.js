6b8dbfd5fcd3f8b8fda9a6f72924ea20dfb6e501
Caitlin Wright <caitlinwright@Caitlins-MacBook-Pro.local>
2021-01-19 19:39:17 -0800

    Day 13 solution added.

---
+++ day13.js
@@ -0,0 +1,17 @@
+// Completed solution here:
+
+// Prompt: 
+
+// Given an array of integers, remove each kth element from it. (Aside: remove every 3rd element in the array.)
+
+// Example:
+// For inputArray = [1,2,3,4,5,6,7,8,9,10] and k=3, the output should be 
+// extractEachKth(inputArray, k) = [1,2,4,5,7,8,10]
+
+// Hints:
+// filter() method creates a new array with all elements that pass the test implemented by the provided function.
+
+function extractEachKth(nums, index) {
+    let numsFiltered = nums.filter(num => num % index != 0) // filters for indices that are not multiples of the index
+    return numsFiltered
+}
